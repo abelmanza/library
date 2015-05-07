ade2740c825bed1d5e814289040c52b68e5c9580
Abel Lorente Ramírez <abel_manza@hotmail.com>
2015-05-07 17:43:41 +0000

    commit

---
+++ app/controllers/library_controller.rb
@@ -0,0 +1,7 @@
+class LibraryController < ApplicationController
+  def index
+    
+    
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'library/index'
+
   resources :books
   devise_for :admins
   devise_for :users
@@ -7,7 +9,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'library#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
